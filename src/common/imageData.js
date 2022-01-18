ab14ad324ad1966cb5263f0b2a846c6642843e67
raghueswar55 <raghueswar55@gmail.com>
2022-01-18 16:53:48 +0530

    added dummy image data

---
+++ src/common/imageData.js
@@ -0,0 +1,46 @@
+export const IMAGES = [
+  null,
+  {
+    title: 'Earlier this morning, NYC',
+    subtitle: 'Lorem ipsum dolor sit amet',
+    uri: 'https://i.imgur.com/UPrs1EWl.jpg',
+    description:
+      'A holiday is a day set aside by custom or by law on which normal activities, especially business or work including school, are suspended or reduced. Generally, holidays are intended to allow individuals to celebrate or commemorate an event or tradition of cultural or religious significance. Holidays may be designated by governments, religious institutions, or other groups or organizations.',
+  },
+  {
+    title: 'Beautiful and dramatic Antelope Canyon',
+    subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
+    uri: 'https://i.imgur.com/l49aYS3l.jpg',
+    description:
+      'A holiday is a day set aside by custom or by law on which normal activities, especially business or work including school, are suspended or reduced. Generally, holidays are intended to allow individuals to celebrate or commemorate an event or tradition of cultural or religious significance. Holidays may be designated by governments, religious institutions, or other groups or organizations.',
+  },
+  {
+    title: 'White Pocket Sunset',
+    subtitle: 'Lorem ipsum dolor sit amet et nuncat ',
+    uri: 'https://i.imgur.com/MABUbpDl.jpg',
+    description:
+      'A holiday is a day set aside by custom or by law on which normal activities, especially business or work including school, are suspended or reduced. Generally, holidays are intended to allow individuals to celebrate or commemorate an event or tradition of cultural or religious significance. Holidays may be designated by governments, religious institutions, or other groups or organizations.',
+  },
+  {
+    title: 'Acrocorinth, Greece',
+    subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
+    uri: 'https://i.imgur.com/KZsmUi2l.jpg',
+    description:
+      'A holiday is a day set aside by custom or by law on which normal activities, especially business or work including school, are suspended or reduced. Generally, holidays are intended to allow individuals to celebrate or commemorate an event or tradition of cultural or religious significance. Holidays may be designated by governments, religious institutions, or other groups or organizations.',
+  },
+  {
+    title: 'The lone tree, majestic landscape of New Zealand',
+    subtitle: 'Lorem ipsum dolor sit amet',
+    uri: 'https://i.imgur.com/2nCt3Sbl.jpg',
+    description:
+      'A holiday is a day set aside by custom or by law on which normal activities, especially business or work including school, are suspended or reduced. Generally, holidays are intended to allow individuals to celebrate or commemorate an event or tradition of cultural or religious significance. Holidays may be designated by governments, religious institutions, or other groups or organizations.',
+  },
+  {
+    title: 'Middle Earth, Germany',
+    subtitle: 'Lorem ipsum dolor sit amet',
+    uri: 'https://i.imgur.com/lceHsT6l.jpg',
+    description:
+      'A holiday is a day set aside by custom or by law on which normal activities, especially business or work including school, are suspended or reduced. Generally, holidays are intended to allow individuals to celebrate or commemorate an event or tradition of cultural or religious significance. Holidays may be designated by governments, religious institutions, or other groups or organizations.',
+  },
+  null,
+];
