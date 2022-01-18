6a06fa843bc503b0123f416c72807976ba670f1f
raghueswar55 <raghueswar55@gmail.com>
2022-01-18 18:02:41 +0530

    implemented visits screen

---
+++ src/screens/holiday/HolidayScreen.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import Carousel from '../../components/carousel';
+import HandleBack from '../../components/HandleBack';
+
+const HolidayScreen = () => {
+  return (
+    <HandleBack>
+      <View style={styles.container}>
+        <View style={styles.flex}>
+          <Carousel />
+        </View>
+      </View>
+    </HandleBack>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  flex: {
+    flex: 1,
+  },
+});
+export default HolidayScreen;
