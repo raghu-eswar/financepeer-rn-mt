5d4906b55555b3d07d1b7683a2dad98866abd996
raghueswar55 <raghueswar55@gmail.com>
2022-01-18 18:37:14 +0530

    added screens to app navigetion stack

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,13 +1,40 @@
 import * as React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from '../screens/home';
+import HolidayScreen from '../screens/holiday';
+import ProfileScreen from '../screens/profile';
+import {
+  CardStyleInterpolators,
+  createStackNavigator,
+} from '@react-navigation/stack';
+import PhotosScreen from '../screens/photos';
+import NotesScreen from '../screens/notes';
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 const AppNavigationStack = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
+    <Stack.Navigator
+      screenOptions={{
+        cardStyleInterpolator: CardStyleInterpolators.forVerticalIOS,
+        headerBackTitle: ' ',
+      }}>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={({navigation, route}) => ({
+          header: props => null,
+        })}
+      />
+      <Stack.Screen
+        name="Holiday"
+        component={HolidayScreen}
+        options={({navigation, route}) => ({
+          header: props => null,
+        })}
+      />
+      <Stack.Screen name="Photos" component={PhotosScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="Notes" component={NotesScreen} />
     </Stack.Navigator>
   );
 };
