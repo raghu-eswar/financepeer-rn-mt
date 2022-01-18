a78280483244a21c67965010beaf6cbfd040e0c8
raghueswar55 <raghueswar55@gmail.com>
2022-01-18 18:26:10 +0530

    initialize database

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-native-paper';
 import {appTheme, COLOR} from './src/common/constants';
 import AppNavigationStack from './src/navigation';
 import {StatusBar} from 'react-native';
+import {DB} from './src/database/db';
 
 const App = () => {
+  useEffect(() => {
+    DB.init();
+  }, []);
   return (
     <Provider theme={appTheme}>
       <StatusBar barStyle="dark-content" backgroundColor={COLOR.white} />
